f2c3a3d882589ed1276cd651d4d2052eb6988cf0
Byron Galindo <bahegasu2000@gmail.com>
2024-10-08 13:49:55 -0500

    Design Class Loader for Loader Console Animation

---
+++ src/Loader.java
@@ -0,0 +1,25 @@
+public class Loader {
+
+    static String loader = "|/-\\";
+
+    public static void startLoader() throws  InterruptedException {
+        startLoader(20, 250, "Loading...  ");
+    }
+
+    public static void startLoader(int ticks, int delay) throws  InterruptedException {
+        startLoader(ticks, delay, "Loading...  ");
+    }
+
+    public static void startLoader(int ticks, int delay, String message) throws InterruptedException {
+
+        System.out.print(message);
+
+        for (int i = 0; i < ticks; i++) {
+            int index = i % loader.length();
+            System.out.print("\r" + message + loader.charAt(index));
+            Thread.sleep(delay);
+        }
+        System.out.println("\nLoad Complete...");
+    }
+
+}
